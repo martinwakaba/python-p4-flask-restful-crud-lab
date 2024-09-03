0d54224814eef24b066fdb398e6e8ed356863d90
MartinWakaba <martin.wakaba@student.moringaschool.com>
2024-09-04 01:11:28 +0300

    done

--- server/models.py
+++ server/models.py
@@ -10,7 +10,7 @@ class Plant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     image = db.Column(db.String)
     price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    is_in_stock = db.Column(db.Boolean, default=True)
 
     def __repr__(self):
         return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
